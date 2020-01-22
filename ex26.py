b3c43d6924c9894929cc56c3adb894d9fcfee261
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:03:30 -0300

    Delete ex26.py

--- ex26.py
+++
@@ -1,5 +0,0 @@
-frase = input('Digite uma frase:').strip()
-
-print('A sua frase possui {} letas "a"'.format(frase.upper().count('A')))
-print('Em que posição a primeira letra "a" se encontra? {}'.format(frase.upper().find('A')+1))
-print('Em que posição a última letra "a" aparece? {}'.format(frase.upper().rfind('A')+1))
