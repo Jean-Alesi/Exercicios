d50e82141f578228a28ee72fefb33eb6e04fcabd
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:01:26 -0300

    Delete ex13.py

--- ex13.py
+++
@@ -1,5 +0,0 @@
-salario = float(input('Salário atual:'))
-aumento = (salario * 15) / 100
-atual = salario + aumento
-
-print('valor do aumento em 15%:\n {}\n valor do salário atual:\n {}'.format(aumento, atual))
