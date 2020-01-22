59c7dba5fa2af039c84797aeebb6a65381eb041e
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 19:59:52 -0300

    Delete ex06.py

--- ex06.py
+++
@@ -1,6 +0,0 @@
-n = int(input('digite um número:'))
-dobro = (n * 2)
-triplo = (n * 3)
-raiz = (n ** (1/2))
-
-print('você digitou o número {} \n o dobro é {} \n o triplo é {} \n e a raiz é {:.2f}'.format(n, dobro, triplo, raiz))
