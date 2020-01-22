a70cd5ba426c4d8ad2cc3b8ff7d7f7c1aba38106
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:00:46 -0300

    Delete ex11.py

--- ex11.py
+++
@@ -1,8 +0,0 @@
-largura = float(input('largura:'))
-altura = float(input('altura:'))
-area = largura * altura
-
-litro = 2
-tinta = area / litro
-
-print('nessa parede será necessário {}L de tinta'.format(tinta))
