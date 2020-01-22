c1638d48458f1fdbe09249224a49625bd7975cc4
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:01:14 -0300

    Delete ex12.py

--- ex12.py
+++
@@ -1,5 +0,0 @@
-prodruto = float(input('digite o valor do produto:'))
-desconto = (prodruto * 5) / 100
-valor = prodruto - desconto
-
-print('Valor a pagar com o desconto aplicado é {:.2f}'.format(valor))
