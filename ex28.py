6cf855b270ffdc45f2c3fcd1a933d84a267b3e85
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:03:40 -0300

    Delete ex28.py

--- ex28.py
+++
@@ -1,9 +0,0 @@
-import random
-num = [0, 1, 2, 3, 4, 5]
-computador = random.choice(num)
-
-jogo = input('Tente adivinhar o número de 0 a 5 que o computador escolheu: ')
-if jogo == computador:
-    print('Você acertou, PARABÉNS!')
-else:
-    print('Você ERROU!')
