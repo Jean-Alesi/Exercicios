2a38fa28cb7696d15d93200526790fc95369fe8e
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 19:59:07 -0300

    Delete ex03.py

--- ex03.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input('primeiro número = '))
-n2 = int(input('segundo número = '))
-s = n1+n2
-#print('A soma é igual {}'.format(s))
-print('A soma entre {}{}{} e {}{}{} é igual a {}{}{}'.format(n1, n2,s))
-
