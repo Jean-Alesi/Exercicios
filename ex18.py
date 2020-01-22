71618f4240c827ad6d3daf61d6776f19ffabd84c
Jean Alesi Gomes de Oliveira <60201632+Jean-Alesi@users.noreply.github.com>
2020-01-22 20:02:21 -0300

    Delete ex18.py

--- ex18.py
+++
@@ -1,9 +0,0 @@
-import math
-
-ang = float(input('Informe o valor do ângulo: '))
-seno = math.sin(math.radians(ang))
-print('O ângulo de {} tem o seno de {:.2f}'.format(ang, seno))
-cosseno = math.cos(math.radians(ang))
-print('O ângulo de {} tem o cosseno de {:.2f}'.format(ang, cosseno))
-tangente = math.tan(math.radians(ang))
-print('O ângulo de {} tem a tangente de {:.2f}'.format(ang,tangente))
